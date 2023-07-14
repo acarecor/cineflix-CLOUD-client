420ce7610b82ec73464d069927315401ea6a751b
Ana Arellano <117821773+acarecor@users.noreply.github.com>
2023-07-14 20:33:24 +0200

    updated images

--- dist/index.b4b6dfad.js
+++ dist/index.b4b6dfad.js
@@ -27175,51 +27175,27 @@ const MainView = ()=>{
             id: 1,
             title: "E.T. the Extraterrestrial",
             description: "After a gentle alien becomes stranded on Earth, he is discovered and befriended by a 10-year-old boy named Elliott. Bringing him into his suburban California house, Elliott introduces E.T., as he is dubbed, to his brother, Michael, and sister, Gertie, and they decide to keep his existence a secret. Soon, however, he falls ill, resulting in government intervention and a dire situation for both him and Elliott.",
-            director: {
-                name: "Steven Spielberg",
-                bio: "A leading figure of the New Hollywood era and pioneer of the modern blockbuster, he is the most commercially successful director in history. He has received numerous awards, including three Academy Awards and two BAFTAs. Seven of his films have been listed in the National Film Registry by the Library of Congress as: culturally, historically or aesthetically significant",
-                birth: "1946-12-18",
-                death: ""
-            },
-            genre: {
-                name: "Adventure",
-                description: "Implies a narrative that is defined by a journey (often including some form of pursuit) and is usually located within a fantasy or exoticized setting. Typically, though not always, such stories include the quest narrative. The predominant emphasis on violence and fighting in action films is the typical difference between the two genres"
-            },
-            image: "https://en.wikipedia.org/wiki/E.T._the_Extra-Terrestrial#/media/File:E_t_the_extra_terrestrial_ver3.jpg",
+            director: "Steven Spielberg",
+            genre: "Adventure",
+            image: "https://upload.wikimedia.org/wikipedia/en/6/66/E_t_the_extra_terrestrial_ver3.jpg",
             year: "1982"
         },
         {
             id: 2,
             title: "The Others",
             description: "A woman named Grace retires with her two children to a mansion on Jersey, towards the end of the Second World War, where she's waiting for her husband to come back from battle",
-            director: {
-                name: "Alejandro Amenabar",
-                bio: "Is the son of a Spanish mother and a Chilean father. His family moved back to Spain when he was 1 year old, and he grew up and studied in Madrid. He wrote, produced and directed his first short film La cabeza at the age of 19, and he was 23 when he directed his feature debut Thesis (1996). His film Open Your Eyes (1997) was a huge success in Spain and was distributed worldwide. It was remade in Hollywood by Cameron Crowe as Vanilla Sky (2001), starring Tom Cruise, Penelope Cruz (also the star of the original version) and Cameron Diaz. The Others (2001) is Amen\xe1bar's first English language film.",
-                birth: "1972-03-31",
-                death: ""
-            },
-            genre: {
-                name: "Thriller",
-                description: "Thriller film, also known as suspense film or suspense thriller, is a broad film genre that evokes excitement and suspense in the audience. The suspense element found in most films' plots is particularly exploited by the filmmaker in this genre. Tension is created by delaying what the audience sees as inevitable, and is built through situations that are menacing or where escape seems impossible.s"
-            },
-            image: "https://en.wikipedia.org/wiki/The_Others_%282001_film%29#/media/File:TheOthers.jpg",
+            director: "Alejandro Amenabar",
+            genre: "Thriller",
+            image: "https://upload.wikimedia.org/wikipedia/en/4/4c/TheOthers.jpg",
             year: "2002"
         },
         {
             id: 3,
             title: "Bicycle Thieves",
             description: "Antonio Ricci, an unemployed man in the depressed post-WWII economy of Italy, finally gets a job hanging up posters, but he needs a bicycle. But when his bicycle is stolen, he and son walk the streets of Rome looking for it. Antonio finally manages to locate the thief, but with no proof he must abandon his cause. But he and his son know perfectly well that without a bike, Antonio won't be able to keep his job.",
-            director: {
-                name: "Vittorio de Sica",
-                bio: "He was an Italian film director and actor, a leading figure in the neorealist movement.Four of the films he directed won Academy Awards: Sciusci\xe0 and Bicycle Thieves (honorary), while Yesterday, Today and Tomorrow and Il giardino dei Finzi Contini won the Academy Award for Best Foreign Language Film. Indeed, the great critical success of Sciusci\xe0 (the first foreign film to be so recognized by the Academy of Motion Picture Arts and Sciences) and Bicycle Thieves helped establish the permanent Best Foreign Film Award. These two films are considered part of the canon of classic cinema. Bicycle Thieves was deemed the greatest film of all time by Sight & Sound magazine's poll of filmmakers and critics in 1958, and was cited by Turner Classic Movies as one of the 15 most influential films in cinema history. De Sica was also nominated for the 1957 Oscar for Best Supporting Actor for playing Major Rinaldi in American director Charles Vidor's 1957 adaptation of Ernest Hemingway's A Farewell to Arms, a movie that was panned by critics and proved a box office flop. De Sica's acting was considered the highlight of the film.",
-                birth: "1901-07-07",
-                death: "1974-11-13"
-            },
-            genre: {
-                name: "Drama",
-                description: "Drama film is a genre that relies on the emotional and relational development of realistic characters. While Drama film relies heavily on this kind of development, dramatic themes play a large role in the plot as well. Often, these dramatic themes are taken from intense, real life issues. Whether heroes or heroines are facing a conflict from the outside or a conflict within themselves, Drama film aims to tell an honest story of human struggles."
-            },
-            image: "https://en.wikipedia.org/wiki/Bicycle_Thieves#/media/File:Ladri3.jpg",
+            director: "Vittorio de Sica",
+            genre: "Drama",
+            image: "https://upload.wikimedia.org/wikipedia/en/2/20/Ladri3.jpg",
             year: "1948"
         }
     ]);
@@ -27229,34 +27205,34 @@ const MainView = ()=>{
         onBackClick: ()=>setSelectedMovie(null)
     }, void 0, false, {
         fileName: "src/components/main-view/main-view.jsx",
-        lineNumber: 64,
+        lineNumber: 40,
         columnNumber: 13
     }, undefined);
     if (movies.length === 0) return /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("div", {
         children: " The list is empty! "
     }, void 0, false, {
         fileName: "src/components/main-view/main-view.jsx",
-        lineNumber: 69,
+        lineNumber: 45,
         columnNumber: 16
     }, undefined);
     return /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("div", {
-        children: movies.map(movie = /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _movieCard.MovieCard), {
-            movie: movie,
-            onMovieClick: (newSelectedMovie)=>{
-                setSelectedMovie(newSelectedMovie);
-            }
-        }, movie.id, false, {
-            fileName: "src/components/main-view/main-view.jsx",
-            lineNumber: 74,
-            columnNumber: 17
-        }, undefined))
+        children: movies.map((movie)=>/*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _movieCard.MovieCard), {
+                movie: movie,
+                onMovieClick: (newSelectedMovie)=>{
+                    setSelectedMovie(newSelectedMovie);
+                }
+            }, movie.id, false, {
+                fileName: "src/components/main-view/main-view.jsx",
+                lineNumber: 50,
+                columnNumber: 17
+            }, undefined))
     }, void 0, false, {
         fileName: "src/components/main-view/main-view.jsx",
-        lineNumber: 72,
+        lineNumber: 48,
         columnNumber: 9
     }, undefined);
 };
-_s(MainView, "/gzgcqGp397fXu4EYflB+vXSwG8=");
+_s(MainView, "11+tUZJnLFmK3/8OSKUQ4lSlxmo=");
 _c = MainView;
 var _c;
 $RefreshReg$(_c, "MainView");
