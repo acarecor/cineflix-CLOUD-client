6eba1f5e373c63c4a5c3b158e5fc3497e4bc660c
Ana Arellano <117821773+acarecor@users.noreply.github.com>
2023-07-14 20:33:36 +0200

    updated images

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -9,7 +9,7 @@ export const MainView = () =>{
         description:"After a gentle alien becomes stranded on Earth, he is discovered and befriended by a 10-year-old boy named Elliott. Bringing him into his suburban California house, Elliott introduces E.T., as he is dubbed, to his brother, Michael, and sister, Gertie, and they decide to keep his existence a secret. Soon, however, he falls ill, resulting in government intervention and a dire situation for both him and Elliott.",
         director: "Steven Spielberg",
         genre: "Adventure",
-        image:"https://en.wikipedia.org/wiki/E.T._the_Extra-Terrestrial#/media/File:E_t_the_extra_terrestrial_ver3.jpg",
+        image:"https://upload.wikimedia.org/wikipedia/en/6/66/E_t_the_extra_terrestrial_ver3.jpg",
         year:"1982",
     
 },
@@ -18,7 +18,7 @@ export const MainView = () =>{
         description:"A woman named Grace retires with her two children to a mansion on Jersey, towards the end of the Second World War, where she's waiting for her husband to come back from battle",
         director: "Alejandro Amenabar",
         genre: "Thriller",
-        image:"https://en.wikipedia.org/wiki/The_Others_%282001_film%29#/media/File:TheOthers.jpg",
+        image:"https://upload.wikimedia.org/wikipedia/en/4/4c/TheOthers.jpg",
         year:"2002",
     
 },
@@ -27,7 +27,7 @@ export const MainView = () =>{
     description: "Antonio Ricci, an unemployed man in the depressed post-WWII economy of Italy, finally gets a job hanging up posters, but he needs a bicycle. But when his bicycle is stolen, he and son walk the streets of Rome looking for it. Antonio finally manages to locate the thief, but with no proof he must abandon his cause. But he and his son know perfectly well that without a bike, Antonio won't be able to keep his job.",
     director: "Vittorio de Sica",
     genre: "Drama",
-    image: "https://en.wikipedia.org/wiki/Bicycle_Thieves#/media/File:Ladri3.jpg",
+    image: "https://upload.wikimedia.org/wikipedia/en/2/20/Ladri3.jpg",
     year: "1948",
    }
 
