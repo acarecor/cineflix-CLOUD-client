81b93d9cc77859137e6d907bb66cecf3b172225e
Ana Arellano <117821773+acarecor@users.noreply.github.com>
2023-07-14 17:54:41 +0200

    updated main view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import { MovieCard } from "../movie-card/movie-card";
+import { MovieView } from "../movie-view/movie-view";
+
 export const MainView = () =>{
     const [ movies, setMovies ] = useState ([
     { id:1,
@@ -53,6 +57,29 @@ export const MainView = () =>{
 
     ]);
 
-    
-}
+    const [selectedMovie, setSelectedMovie] = useState (null);
+
+    if (selectedMovie) {
+        return (
+            <MovieView movie={selectedMovie} onBackClick={()=> setSelectedMovie(null)} />
+        );
+    }
+
+    if(movies.length === 0 ) {
+        return <div> The list is empty </div>;
+    }
+    return (
+        <div>
+            {movies.map((movie) = (
+                <MovieCard
+                key = {movie.id}
+                movie = {movie}
+                onMovieClick={(newSelectedMovie) => {
+                    setSelectedMovie(newSelectedMovie);
+                }}
+                />
+            ))}
+        </div>
+    );
+};
 
