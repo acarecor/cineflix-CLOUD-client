1bc42e00b6dca3d0cb620e3c7dc539b26f11311d
Ana Arellano <117821773+acarecor@users.noreply.github.com>
2023-07-17 20:29:49 +0200

    loaded data from the APi

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
@@ -6,6 +6,29 @@ export const MainView = () =>{
     const [ movies, setMovies ] = useState ([]);
     
     const [selectedMovie, setSelectedMovie] = useState (null);
+
+    useEffect(()=> {
+        fetch("https://myflix-movies-2a93844126ef.herokuapp.com")
+        .then ((response)=> response.json())
+        .then((moviesData) => {
+            const moviesFromApi = moviesData.movies.map((movie) => {
+                return {
+                    id: movie.key,
+                    image: movie.imagePath,
+                    title: movie.title,
+                    description:movie.description,
+                    director: movie.director.name,
+                    bio: movie.director.bio,
+                    birth:movie.director.birth,
+                    death:movie.director.death?.[0],
+                    genre: movie.genre.name,
+                    description:movie.genre.description,
+                    year: movie.year,
+                };
+            });
+            setMovies(moviesFromApi);
+        });
+    }, []);
 
     if (selectedMovie) {
         return (
