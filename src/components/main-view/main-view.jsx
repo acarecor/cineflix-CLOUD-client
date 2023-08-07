7eb4f000c3ff0307e94d7f31d9d7a018cc1dfca3
Ana Arellano <117821773+acarecor@users.noreply.github.com>
2023-08-07 20:31:33 +0200

    Squashed commit of the following:
    commit 8c27e42a83fea9cb96ab4649a3be6e9757bc4789
    Merge: 130526d f099005
    Author: Ana Arellano <117821773+acarecor@users.noreply.github.com>
    Date:   Mon Aug 7 20:24:41 2023 +0200
    
        Merge branch 'main' into login-and-auth
    
    commit 130526df9aa9c39253960ea53afd393dcc483eba
    Author: Ana Arellano <117821773+acarecor@users.noreply.github.com>
    Date:   Thu Jul 20 18:01:33 2023 +0200
    
        updated main view
    
    commit a72dbd95a9b4a6f175834a740dd6b6988411aa70
    Author: Ana Arellano <117821773+acarecor@users.noreply.github.com>
    Date:   Thu Jul 20 17:25:33 2023 +0200
    
        updated const data
    
    commit 275a35f25726f71a435423511d600c8c49819905
    Merge: f807dc8 b785c69
    Author: Ana Arellano <117821773+acarecor@users.noreply.github.com>
    Date:   Thu Jul 20 14:57:24 2023 +0200
    
        Merge branch 'login-and-auth' of https://github.com/acarecor/myFlix-client into login-and-auth
    
    commit f807dc8f61d0a3d0ff0b4086f87a0b19841582ac
    Author: Ana Arellano <117821773+acarecor@users.noreply.github.com>
    Date:   Thu Jul 20 14:51:17 2023 +0200
    
        updated login
    
    commit 5cec4c1521763a1526156e9f66f2260cad891466
    Author: Ana Arellano <117821773+acarecor@users.noreply.github.com>
    Date:   Thu Jul 20 14:51:04 2023 +0200
    
        updated login-view
    
    commit 9e6411a3288275c7b17e103f9948556507279d98
    Author: Ana Arellano <117821773+acarecor@users.noreply.github.com>
    Date:   Thu Jul 20 14:49:20 2023 +0200
    
        updated main view
    
    commit b785c69051ad5c595a2b2a88efee1182435a9984
    Author: Ana Arellano <117821773+acarecor@users.noreply.github.com>
    Date:   Thu Jul 20 12:59:54 2023 +0200
    
        Delete .DS_Store
    
    commit 2f938e031a9e03589dab75f8b11a07593e216c95
    Author: Ana Arellano <117821773+acarecor@users.noreply.github.com>
    Date:   Thu Jul 20 12:41:47 2023 +0200
    
        updated main view
    
    commit 9f6c40ef2f4cc16302219bc1445d34f0601ee5f1
    Author: Ana Arellano <117821773+acarecor@users.noreply.github.com>
    Date:   Thu Jul 20 11:40:48 2023 +0200
    
        updated login view
    
    commit 578e925e40e559773579123a2c8386d8aea2e399
    Author: Ana Arellano <117821773+acarecor@users.noreply.github.com>
    Date:   Thu Jul 20 11:40:24 2023 +0200
    
        created signup view
    
    commit 376547703b40c688c8c65d1ab510b249fb13d25c
    Author: Ana Arellano <117821773+acarecor@users.noreply.github.com>
    Date:   Thu Jul 20 11:03:04 2023 +0200
    
        created signup view
    
    commit 73fbabb9199e30955a102152456e64c779ca9389
    Author: Ana Arellano <117821773+acarecor@users.noreply.github.com>
    Date:   Thu Jul 20 10:58:03 2023 +0200
    
        added local storage
    
    commit fdc0a9a54b3374b4e5daf65139df8d7a6a92d17b
    Author: Ana Arellano <117821773+acarecor@users.noreply.github.com>
    Date:   Thu Jul 20 10:36:40 2023 +0200
    
        added token auth
    
    commit d49df6bb22f7c65bd93861036307bf799577ea80
    Author: Ana Arellano <117821773+acarecor@users.noreply.github.com>
    Date:   Thu Jul 20 10:36:04 2023 +0200
    
        added token authentication
    
    commit 8ea5ebb94e13bbc1125e675854d5b19c17ec568d
    Author: Ana Arellano <117821773+acarecor@users.noreply.github.com>
    Date:   Thu Jul 20 09:50:23 2023 +0200
    
        updated URL of the API
    
    commit 6ae4b68721cb5a25b5cc2cb4b8d96e5b9e7e6a85
    Author: Ana Arellano <117821773+acarecor@users.noreply.github.com>
    Date:   Thu Jul 20 09:30:59 2023 +0200
    
        created logout button
    
    commit 925707dd30ad2667e2bf0f20bf4e1b04d5a8a694
    Author: Ana Arellano <117821773+acarecor@users.noreply.github.com>
    Date:   Thu Jul 20 09:28:00 2023 +0200
    
        imported LoginView
    
    commit 38776bbf307e6e246e549ac8c32201388ee53f1e
    Author: Ana Arellano <117821773+acarecor@users.noreply.github.com>
    Date:   Thu Jul 20 08:58:30 2023 +0200
    
        created login-view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,17 +1,28 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () =>{
-    const [ movies, setMovies ] = useState ([]);
-    
+    const storedUser = JSON.parse(localStorage.getItem ("user"));
+    const storedToken =localStorage.getItem("token");
+    const [user,setUser] = useState (null);
+    const [token, setToken] =useState(null);
+    const [movies, setMovies ] = useState ([]); 
     const [selectedMovie, setSelectedMovie] = useState (null);
 
     useEffect(()=> {
-        fetch("https://myflix-movies-2a93844126ef.herokuapp.com/movies")
+        if(!token){
+            return;
+        }
+        fetch("https://myflix-movies-2a93844126ef.herokuapp.com/movies",
+        {
+            headers: { Authorization: `Bearer ${token}` }
+        })
         .then ((response)=> response.json())
-        .then((moviesData) => {
-            const moviesFromApi = moviesData.map((movie) => {
+        .then((data) => {
+            const moviesFromApi = data.map((movie) => {
                 return {
                     id: movie._id,
                     imagePath: movie.imagePath,
@@ -33,11 +44,41 @@ export const MainView = () =>{
             });
             setMovies(moviesFromApi);
         });
-    }, []);
+    }, [token]);
+
+    if (!user){
+        return (
+            <>
+                <LoginView 
+                    onLoggedIn={(user,token)=> {
+                        setUser (user);
+                        setToken(token);
+                    }} 
+                />
+                or
+                <SignupView />
+            </>
+        );
+    }
 
     if (selectedMovie) {
         return (
-            <MovieView movie={selectedMovie} onBackClick={()=> setSelectedMovie(null)} />
+            <>
+                <button
+                    onClick={() => {
+                        setUser(null);
+                        setToken(null);
+                        localStorage.clear();
+                    }}
+                >
+                    Logout
+                </button>
+                
+                <MovieView 
+                    movie={selectedMovie} 
+                    onBackClick={()=> setSelectedMovie(null)} 
+                />
+            </>
         );
     }
 
@@ -46,6 +87,16 @@ export const MainView = () =>{
     }
     return (
         <div>
+            
+                <button
+                    onClick={() => {
+                        setUser(null);
+                        setToken(null);
+                        localStorage.clear();
+                    }}
+                >
+                    Logout
+                </button>
             {movies.map((movie) => (
                 <MovieCard
                 key = {movie.id}
