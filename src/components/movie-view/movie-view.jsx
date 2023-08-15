b417b320ca6ee9843a6852b9d659ed4b64ea9d31
Ana Arellano <117821773+acarecor@users.noreply.github.com>
2023-08-16 00:12:33 +0200

    removed addFav and removeFav from Movie-view

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -5,62 +5,7 @@ import { Link } from "react-router-dom";
 
 export const MovieView = ({ movies, user, setUser, token }) => {
   const { movieId } = useParams();
-  const [favoritesMovies, setFavoritesMovies] = useState(false);
-
-  useEffect(() => {
-    if (user.favoritesMovies && user.favoritesMovies.includes(movieId)) {
-      setFavoritesMovies(true);
-    }
-  }, []);
-
-  const addFav = (event) => {
-    event.preventDefault();
-    fetch(
-      `https://myflix-movies-2a93844126ef.herokuapp.com/users/${user.username}/movies/${movieId}`,
-      {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        setFavoritesMovies(true);
-        user.favoritesMovies.push(movieId);
-        localStorage.setItem("user", JSON.stringify(user));
-        setUser(user);
-      })
-      .catch((error) => {
-        alert("Something is wrong");
-      });
-  };
-
-  const removeFav = () => {
-    fetch(
-      `https://myflix-movies-2a93844126ef.herokuapp.com/users/${user.username}/movies/${movieId}`,
-      {
-        method: "DELETE",
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        setFavoritesMovies(false);
-        user.favoritesMovies = user.favoritesMovies.filter(
-          (id) => id !== movieId
-        );
-        localStorage.setItem("user", JSON.stringify(user));
-        setUser(user);
-        alert("movie was deleted from your list!");
-      })
-      .catch((error) => {
-        alert("Something is wrong");
-      });
-  };
-
+  
   const movie = movies.find((b) => b.id === movieId);
 
   return (
@@ -78,25 +23,7 @@ export const MovieView = ({ movies, user, setUser, token }) => {
         <Card.Text>{movie.genre.description}</Card.Text>
         <Card.Text>Year: {movie.year}</Card.Text>
         <Row>
-          <Col>
-            {favoritesMovies ? (
-              <Button
-                variant="primary"
-                onClick={removeFav}
-                style={{ cursor: "pointer" }}
-              >
-                Remove from Favorites
-              </Button>
-            ) : (
-              <Button
-                variant="primary"
-                onClick={addFav}
-                style={{ cursor: "pointer" }}
-              >
-                Add to Favorites
-              </Button>
-            )}
-          </Col>
+          
           <Col className="d-flex justify-content-right">
             <Link to={`/`}>
               <Button variant="primary" style={{ cursor: "pointer" }}>
