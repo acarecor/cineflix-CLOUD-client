774b7299f1ce61bc72c4434ee9d02221b2a3f9a8
Ana Arellano <117821773+acarecor@users.noreply.github.com>
2023-08-16 12:17:49 +0200

    added accordion and change movie view

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
-import { Button, Card, Row, Col } from "react-bootstrap";
+import { Button, Card, Row, Col, Accordion } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { GoPlus} from "react-icons/go";
 
 export const MovieView = ({ movies, user, setUser, token }) => {
   const { movieId } = useParams();
@@ -13,15 +14,25 @@ export const MovieView = ({ movies, user, setUser, token }) => {
       <Card.Img variant="top" src={movie.imagePath} />
 
       <Card.Body>
-        <Card.Title>Title: {movie.title}</Card.Title>
+        <Card.Title> {movie.title}</Card.Title>
         <Card.Title>Description:</Card.Title>
         <Card.Text>{movie.description}</Card.Text>
-        <Card.Title>Director: {movie.director.name}</Card.Title>
-        <Card.Text> {movie.director.bio}</Card.Text>
-        <Card.Text> Birth: {movie.director.birth}</Card.Text>
-        <Card.Title> Genre: {movie.genre.name}</Card.Title>
-        <Card.Text>{movie.genre.description}</Card.Text>
-        <Card.Text>Year: {movie.year}</Card.Text>
+        <Accordion defaultActiveKey="0">
+          <Accordion.Item eventKey="1">
+            <Accordion.Header>Director: {movie.director.name}</Accordion.Header>
+             <Accordion.Body>
+             <Card.Text> {movie.director.bio}</Card.Text>
+             <Card.Text> Birth: {movie.director.birth}</Card.Text>
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="2">
+            <Accordion.Header>Genre: {movie.genre.name}</Accordion.Header>
+             <Accordion.Body>
+             <Card.Text> {movie.genre.description}</Card.Text>
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+        <Card.Text>Realese Year: {movie.year}</Card.Text>
         <Row>
           
           <Col className="d-flex justify-content-right">
